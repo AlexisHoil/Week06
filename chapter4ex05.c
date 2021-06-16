a444168cf38064d2c77886fb722259b315563fc0
AlexisHoil <84060909+AlexisHoil@users.noreply.github.com>
2021-06-15 21:40:04 -0500

    Create chapter4ex05.c

---
+++ chapter4ex05.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int int1;
+char char1;
+float float1;
+
+int main(void) {
+
+int1= 3;
+char1= 'L';
+float1= 1.0;
+ 
+  printf("Floating-point number is %d\n",float1);
+  printf("integer number is %f\n",var1);
+  printf("the character is %d\n",char1);
+  
+ 
+  return 0;
+}
