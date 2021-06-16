4349ff0758b31edfd9e12a1e532e822e4dd5daa6
AlexisHoil <84060909+AlexisHoil@users.noreply.github.com>
2021-06-15 20:07:09 -0500

    Create chapter4ex03.c

---
+++ chapter4ex03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int perimeter;
+int area;
+
+int main(void) {
+
+perimeter= (2*3)+(2*5);
+area= 3*5;
+
+  printf("the perimeter is %d in\n", perimeter);
+  printf("the area is %d in\n", area);
+ 
+  return 0;
+}
