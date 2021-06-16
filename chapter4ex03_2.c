8fe8a1148de2d9980518fa01e7ce1e05e483c0e2
AlexisHoil <84060909+AlexisHoil@users.noreply.github.com>
2021-06-15 20:15:13 -0500

    Create chapter4ex03_2.c

---
+++ chapter4ex03_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float perimeter;
+float area;
+
+int main(void) {
+
+perimeter= (2.0*6.8)+(2.0*2.3);
+area= 6.8*2.3;
+
+  printf("the perimeter is %f in\n", perimeter);
+  printf("the area is %f in\n", area);
+ 
+  return 0;
+}
