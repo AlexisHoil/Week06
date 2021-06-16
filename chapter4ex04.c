f07e2c412ccba40dfd750655b1c9c8675317f5ee
AlexisHoil <84060909+AlexisHoil@users.noreply.github.com>
2021-06-15 20:51:32 -0500

    Create chapter4ex04.c

---
+++ chapter4ex04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+  printf("H   H EEEEE L      OOO\n");
+  printf("H   H E     L     O   O\n");
+  printf("H   H E     L     O   O\n");
+  printf("HHHHH EEEEE L     O   O\n");
+  printf("H   H E     L     O   O\n");
+  printf("H   H E     L     O   O\n");
+  printf("H   H EEEEE LLLLL  OOO\n");
+  
+ 
+  return 0;
+}
