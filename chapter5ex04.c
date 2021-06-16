5d3a0c6fb3849717f0a331da92ea7a03b69127db
AlexisHoil <84060909+AlexisHoil@users.noreply.github.com>
2021-06-15 23:56:34 -0500

    Create chapter5ex04.c

---
+++ chapter5ex04.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+char line[100];
+float kilometer;
+float convert;
+
+
+int main(void) {
+
+
+  printf("Enter kilometers per hour with maximun 2 decimals: ");
+  fgets(line, sizeof(line), stdin);
+  sscanf(line, "%f", &kilometer);
+  
+  convert= kilometer/0.6213712;
+  printf("The %.2f kilometers per hour is equal to %.2f miles", kilometer, convert);
+ 
+  return 0;
+}
